42e1e02b600104c60b25df39c1d37d8fd42db3e3
chaoyuy <57982332+chaoyuy@users.noreply.github.com>
2021-10-24 22:56:36 +1100

    add search sql

---
+++ search.sql
@@ -0,0 +1,11 @@
+INSERT INTO 'search'('Id','Name','Email') VALUES
+(1,'John Doe','John@Doe.com'),
+(2,'Jane Doe','Jane@Doe.com'),
+(3,'Apple Doe','Apple@Doe.com'),
+(4,'Beck Doe','Beck@Doe.com'),
+(5,'Charlie Doe','Charlie@Doe.com'),
+(6,'Dion Doe','Dion@Doe.com'),
+(7,'Peter Doe','Peter@Doe.com'),
+(8,'Chaoyu Doe','Chaoyu@Doe.com'),
+(9,'Yuyu Doe','Yuyu@Doe.com'),
+(10,'Jill Doe','Jill@Doe.com');
